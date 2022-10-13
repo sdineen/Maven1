9969a7e18cbb698deac896c850e641c01f10cac4
sdineen <dineen701@gmail.com>
2022-10-13 09:23:28 +0100

    thu

--- src/main/java/com/qa/week2/thursday/repository/ProductRepository.java
+++ src/main/java/com/qa/week2/thursday/repository/ProductRepository.java
@@ -5,17 +5,5 @@ import java.util.List;
 import com.qa.week2.thursday.domain.Product;
 
 public interface ProductRepository {
-    long create(Product product);
 
-    List<Product> selectAll();
-
-    Product findById(long id);
-
-    List<Product> findByNameContaining(String partOfName);
-
-    List<Product> findByRetailPriceLessThanEqual(double max);
-
-    boolean update(Product product);
-
-    boolean deleteById(Long id);
 }
